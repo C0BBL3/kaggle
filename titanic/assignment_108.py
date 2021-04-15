7b0ec0512166ca07bba0128ba92558a9eb8e4d70
Colby Roberts <>
2021-04-15 06:20:58 +0000

    Assignment 108 Completed

--- titanic/assignment_108.py
+++ titanic/assignment_108.py
@@ -1,109 +1,53 @@
 import numpy as np
+import pandas as pd
+import os
 from sklearn.linear_model import LinearRegression
-from parse_line import parse_line 
-import sys
-sys.path.append('src/models')
-from dataframe import DataFrame
-from logistic_regressor import LogisticRegressor
-from linear_regressor import LinearRegressor
 
-data_types = {
-    "PassengerId": int,
-    "Survived": int,
-    "Pclass": int,
-    "Name": str,
-    "Sex": str,
-    "Age": float,
-    "SibSp": int,
-    "Parch": int,
-    "Ticket": str,
-    "Fare": float,
-    "Cabin": str,
-    "Embarked": str
-}
+dataframe = pd.read_csv(os.getcwd() + '/titanic/dataset_of_knowns.csv')
+keep_cols = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked']
+dataframe = dataframe[keep_cols]
+dataframe['Sex'] = dataframe['Sex'].apply(lambda sex: 1 if sex == 'female' else 0)
+age_nan = dataframe['Age'].apply(lambda entry: np.isnan(entry))
+age_not_nan = dataframe['Age'].apply(lambda entry: not np.isnan(entry))
+dataframe.loc[age_nan, ['Age']] = dataframe['Age'][age_not_nan].mean()
+dataframe['SibSp>0'] = dataframe['SibSp'].apply(lambda sibsp: 1 if sibsp > 0 else 0)
+dataframe['Parch>0'] = dataframe['Parch'].apply(lambda parch: 1 if parch > 0 else 0)
+dataframe['Cabin']= dataframe['Cabin'].fillna('None')
+dataframe['CabinType'] = dataframe['Cabin'].apply(lambda cabin: cabin[0] if cabin != 'None' else cabin)
+for cabin_type in dataframe['CabinType'].unique():
+    dummy_variable_name = 'CabinType={}'.format(cabin_type)
+    dummy_variable_values = dataframe['CabinType'].apply(lambda entry: int(entry == cabin_type))
+    dataframe[dummy_variable_name] = dummy_variable_values
+del dataframe['CabinType']
+dataframe['Embarked'] = dataframe['Embarked'].fillna('None')
+for embarked_type in dataframe['Embarked'].unique():
+    dummy_variable_name = 'Embarked={}'.format(embarked_type)
+    dummy_variable_values = dataframe['Embarked'].apply(lambda entry: int(entry == embarked_type))
+    dataframe[dummy_variable_name] = dummy_variable_values
+del dataframe['Embarked']
 
+ratings = dataframe['Survived']
+features_to_use = ['Sex', 'Pclass', 'Fare', 'Age', 'SibSp', 'SibSp>0', 'Parch>0', 'Embarked=C', 'Embarked=None', 'Embarked=Q', 'Embarked=S', 'CabinType=A', 'CabinType=B', 'CabinType=C', 'CabinType=D', 'CabinType=E', 'CabinType=F', 'CabinType=G', 'CabinType=None', 'CabinType=T']
+dataframe = dataframe[features_to_use]
+print("\n", dataframe)
+training_ratings = ratings[:500]
+testing_ratings = ratings[500:]
+training_dataframe = dataframe[:500]
+testing_dataframe = dataframe[500:]
+training_x = np.array(training_dataframe)
+training_y = np.array(training_ratings)
 
-dataframe = DataFrame.from_csv("kaggle/titanic/dataset_of_knowns.csv", data_types=data_types, parser=parse_line)
+coeffs = LinearRegression().fit(training_x, training_y)
+print("Constant", coeffs.intercept_)
+final_results = {column: coefficient for column, coefficient in zip(training_dataframe.columns, coeffs.coef_)}
+for col,value in final_results.items():
+    print(col,value)
+print("\n")
 
-dataframe.apply('Survived', lambda i: float(i))
-dataframe.apply('Sex', lambda sex: 1 if sex == 'female' else 0)
-fixed_age = [element for element in dataframe['Age'] if element is not None]
-dataframe.apply('Age', lambda i: float(i) if i != '' and i is not None else sum(fixed_age) / len(fixed_age))
-dataframe.apply('Pclass', lambda i: float(i))
-dataframe.apply('SibSp', lambda i: float(i))
-dataframe.apply('Parch', lambda i: float(i))
-dataframe.apply('Embarked', lambda i: 0.0 if i == 'S' else 1.0 if i == 'C' else 2.0 if i == 'Q' else 0.0)
-copy_df = DataFrame({key: value for key, value in dataframe.data_dict.items()}, dataframe.columns)
-dataframe.append_columns({'SibSp=0': [element for element in dataframe['SibSp'] if element == 0]})
-dataframe.append_columns({'SibSp>=1': [element for element in dataframe['SibSp'] if element >= 1]})
-dataframe.append_columns({'Parch=0': [element for element in dataframe['Parch'] if element == 0]})
-dataframe.append_columns({'Cabin=A': [element for element in dataframe['Cabin'] if element == 'A']})
-dataframe.append_columns({'Cabin=B': [element for element in dataframe['Cabin'] if element == 'B']})
-dataframe.append_columns({'Cabin=C': [element for element in dataframe['Cabin'] if element == 'C']})
-dataframe.append_columns({'Cabin=D': [element for element in dataframe['Cabin'] if element == 'D']})
-dataframe.append_columns({'Cabin=E': [element for element in dataframe['Cabin'] if element == 'E']})
-dataframe.append_columns({'Cabin=F': [element for element in dataframe['Cabin'] if element == 'F']})
-dataframe.append_columns({'Cabin=G': [element for element in dataframe['Cabin'] if element == 'G']})
-dataframe.append_columns({'Cabin=T': [element for element in dataframe['Cabin'] if element == 'T']})
-dataframe.append_columns({'Cabin=None': [element for element in dataframe['Cabin'] if element is None]})
-dataframe.append_columns({'Embarked=S': [element for element in dataframe['Embarked'] if element == 'S']})
-dataframe.append_columns({'Embarked=C': [element for element in dataframe['Embarked'] if element == 'C']})
-dataframe.append_columns({'Embarked=Q': [element for element in dataframe['Embarked'] if element == 'Q']})
-dataframe.append_columns({'Embarked=None': [element for element in dataframe['Embarked'] if element is None]})
-dataframe.append_columns({'indices': [index for index in range(len(dataframe))]}) #indices for counting
-dataframe.append_columns({'constant': [1 for _ in range(len(dataframe))]}) #constant
-dataframe.append_columns({'SibSp': copy_df['SibSp']})
-dataframe.append_columns({'Parch': copy_df['Parch']})
-dataframe.append_columns({'Embarked': copy_df['Embarked']})
-dataframe.remove_columns(['PassengerId', 'Ticket', 'Cabin', 'Name', 'Embarked'])
+predictions = coeffs.predict(testing_dataframe)
+fixed_predictions = [1 if output > 0.5 else 0 for output in predictions]
+result = [prediction == actual for prediction, actual in zip(predictions, testing_dataframe['Survived'])].count(True) / len(fixed_predictions)
+print("accuracy", result)
 
-dataframes = []
-training_dataframe = dataframe.select_rows_where(lambda row: row['indices'] in list(range(0, 500)))
-testing_dataframe = dataframe.select_rows_where(lambda row: row['indices'] in list(range(500, len(dataframe))))
+ 
 
-def get_classifications(model, dataframe):
-    classifications = []
-    for index in range(len(dataframe)):
-        observation = {column: True if array[index] >= array[len(array)//2] else False for column, array in dataframe.data_dict.items() if column != 'indices'}
-        guess = model.predict(observation)
-        if guess >= 0.5:
-            guess = 1
-        else:
-            guess = 0
-        classifications.append((testing_dataframe[index], guess))
-    return classifications
-
-'''
-dataframes.append(dataframe.select(['Sex', 'Survived', 'indices']).group_by('Sex').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Sex', 'ASC']]))
-dataframes.append(dataframe.select(['Age', 'Survived', 'indices']).group_by('Age').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Age', 'ASC']]))
-dataframes.append(dataframe.select(['SibSp', 'Survived', 'indices']).group_by('SibSp').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['SibSp', 'ASC']]))
-dataframes.append(dataframe.select(['Parch', 'Survived', 'indices']).group_by('Parch').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Parch', 'ASC']]))
-dataframes.append(dataframe.select(['Cabin', 'Survived', 'indices']).group_by('Cabin').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Cabin', 'ASC']])) 
-dataframes.append(dataframe.select(['Embarked', 'Survived', 'indices']).group_by('Embarked').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Embarked', 'ASC']]))
-dataframes.append(dataframe.select(['Pclass', 'Survived', 'indices']).group_by('Pclass').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Pclass', 'ASC']]))
-dataframes.append(dataframe.select(['Fare', 'Survived', 'indices']).group_by('Fare').aggregate('indices', 'count').aggregate('Survived', 'avg').order_by([['Fare', 'ASC']]))
-'''
-
-sex_training_dataframe = training_dataframe.select_columns(['Survived', 'Sex', 'constant'])
-sex_ratings = [[survived] for survived in sex_training_dataframe['Survived']]
-sex_training_dataframe.remove_columns('Survived')
-sex_training_x = np.array(sex_training_dataframe.to_array())
-sex_training_y = np.array(sex_ratings)
-sex_coeffs = LinearRegression().fit(sex_training_x, sex_training_y)
-print(sex_coeffs.score(sex_training_x, sex_training_y))
-
-sex_pclass_training_dataframe = training_dataframe.select_columns(['Survived', 'Sex', 'Pclass', 'constant'])
-sex_pclass_ratings = [[survived] for survived in sex_pclass_training_dataframe['Survived']]
-sex_pclass_training_dataframe.remove_columns('Survived')
-sex_pclass_training_x = np.array(sex_pclass_training_dataframe.to_array())
-sex_pclass_training_y = np.array(sex_pclass_ratings)
-sex_pclass_coeffs = LinearRegression().fit(sex_pclass_training_x, sex_pclass_training_y)
-print(sex_pclass_coeffs.score(sex_training_x, sex_training_y))
-
-sex_pclass_dot_dot_dot_training_dataframe = training_dataframe.select_columns(['Survived', 'Sex', 'Pclass', 'Fare', 'Age', 'SibSp', 'SibSp=0', 'Parch=0', 'constant'])
-sex_pclass_dot_dot_dot_ratings = [[survived] for survived in sex_pclass_dot_dot_dot_training_dataframe['Survived']]
-sex_pclass_dot_dot_dot_training_dataframe.remove_columns('Survived')
-sex_pclass_dot_dot_dot_training_x = np.array(sex_pclass_dot_dot_dot_training_dataframe.to_array())
-sex_pclass_dot_dot_dot_training_y = np.array(sex_pclass_dot_dot_dot_ratings)
-sex_pclass_dot_dot_dot_coeffs = LinearRegression().fit(sex_pclass_dot_dot_dot_training_x, sex_pclass_dot_dot_dot_training_y)
-print(sex_pclass_dot_dot_dot_coeffs.score(sex_training_x, sex_training_y))
